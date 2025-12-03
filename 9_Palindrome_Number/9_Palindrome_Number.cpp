97c41ffef4a2ac6fd3e3e725a60a7d2b7484f646
Mitsutaka Shiraishi <srismttk@gmail.com>
2025-12-03 23:07:02 +0900

    9_Palindrome_Number: first implementation passed tests.

--- 9_Palindrome_Number/9_Palindrome_Number.cpp
+++ 9_Palindrome_Number/9_Palindrome_Number.cpp
@@ -1,6 +1,38 @@
+#include <stdio.h>
+#include <cmath>
+
 class Solution {
 public:
+    int getDigitNum(int x) {
+        int digitNum = 0;
+        int temp = x;
+        while(temp / 10 > 0) {
+            digitNum++;
+            temp /= 10;
+        }
+        return digitNum;
+    }
+
     bool isPalindrome(int x) {
+        if (x < 0) return false;
+        if ((x >= 0) && (x <= 9)) return true;
+
+        int digitNum = getDigitNum(x);
+        int left = 1;
+        int right = std::pow(10, digitNum);
+
+        while (left < right) {
+            int leftDigit = (x / left) % 10 ;
+            int rightDigit = (x / right) % 10 ;
+            printf("leftDigit: %d, rightDigit: %d\n", leftDigit, rightDigit);
+            if (leftDigit != rightDigit) {
+                return false;
+            }
+
+            left = left * 10;
+            right = right / 10;
+        }
+
         return true;
     }
 };
