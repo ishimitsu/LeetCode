c27ad3ad4e83d404d99fe143e16218d764e6a2dd
Mitsutaka Shiraishi <srismttk@gmail.com>
2025-12-09 01:35:23 +0900

    first implementation, it is O(n^3) so need improvement.

--- 15_3Sum/3Sum.cpp
+++ 15_3Sum/3Sum.cpp
@@ -6,8 +6,53 @@ using std::vector;
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // TODO: implement
-        return {};
+        vector<vector<int>> result = {};
+
+        for(int fst = 0; fst < nums.size(); fst++)  {
+            for(int sec = fst + 1; sec < nums.size(); sec++)  {
+                for(int thd = sec + 1; thd < nums.size(); thd++)  {
+
+                    if (nums[fst] + nums[sec] + nums[thd] == 0) {
+                        bool duplicated = false;
+                        for (vector<int> vec : result) {
+                            vector<bool> duplicated_three = {false, false, false};
+                            int duplicated_cnt = 0;
+
+                            for (int i = 0; i < vec.size(); i++) {
+                                if (!duplicated_three[0] && nums[fst] == vec[i]) {
+                                    duplicated_three[0] = true;
+                                    duplicated_cnt++;
+                                } else if (!duplicated_three[1] && nums[sec] == vec[i]) {
+                                    duplicated_three[1] = true;
+                                    duplicated_cnt++;
+                                } else if (!duplicated_three[2] && nums[thd] == vec[i]) {
+                                    duplicated_three[2] = true;
+                                    duplicated_cnt++;
+                                }
+
+                                if (duplicated_cnt >= 3) {
+                                    duplicated = true;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (!duplicated) {
+                            vector<int> three_sum = {-1, -1, -1};
+                            three_sum[0] = nums[fst];
+                            three_sum[1] = nums[sec];
+                            three_sum[2] = nums[thd];
+
+                            result.push_back(three_sum);
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+        return result;
     }
 };
 
