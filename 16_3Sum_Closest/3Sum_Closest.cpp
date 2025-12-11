95dd6a88bbf2176da994bb11dcb9b95da588bf18
Mitsutaka Shiraishi <srismttk@gmail.com>
2025-12-11 22:15:14 +0900

    implement threeSumClosest that passes.

--- 16_3Sum_Closest/3Sum_Closest.cpp
+++ 16_3Sum_Closest/3Sum_Closest.cpp
@@ -12,8 +12,36 @@ public:
     // Return the sum of the three integers.
     // You may assume that each input would have exactly one solution.
     int threeSumClosest(vector<int>& nums, int target) {
-        // TODO: implement
-        return 0;
+        int result = 10001; // -10^4 <= target <= 10^4
+        int resultDiff = 20002; // -10^4 ~ 10^4
+        std::sort(nums.begin(), nums.end());
+
+        for(int fst = 0; fst < nums.size(); fst++)  {
+            if(fst > 0 && nums[fst] == nums[fst - 1]) continue; // fst is duplicated, skip
+
+            int left = fst + 1;
+            int right = nums.size() - 1;
+            while (left < right) {
+                int threeSum = nums[fst] + nums[left] + nums[right];
+
+                if (threeSum == target) return target;
+
+                int threeSumDiff = std::abs(target - threeSum);
+                if (threeSumDiff < resultDiff) {
+                    resultDiff = threeSumDiff;
+                    result = threeSum;
+
+                    // skip duplicated left/right
+                    //while(left < right && nums[left] == nums[left + 1]) left++;
+                    //while(left < right && nums[right] == nums[right - 1]) right--;
+                }
+
+                if (threeSum > target) { right--; }
+                else { left++; }
+            }
+        }
+
+        return result;
     }
 };
 
@@ -24,23 +52,36 @@ protected:
     Solution solution;
 };
 
-// Example 1: nums = [-1,2,1,-4], target = 1
-// Output: 2
-// Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 TEST_F(ThreeSumClosestTest, Example1) {
     vector<int> nums = {-1, 2, 1, -4};
     int target = 1;
     EXPECT_EQ(solution.threeSumClosest(nums, target), 2);
 }
 
-// Example 2: nums = [0,0,0], target = 1
-// Output: 0
-// Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
 TEST_F(ThreeSumClosestTest, Example2) {
     vector<int> nums = {0, 0, 0};
     int target = 1;
     EXPECT_EQ(solution.threeSumClosest(nums, target), 0);
 }
+
+TEST_F(ThreeSumClosestTest, Example3) {
+    vector<int> nums = {4,0,5,-5,3,3,0,-4,-5};
+    int target = -2;
+    EXPECT_EQ(solution.threeSumClosest(nums, target), -2);
+}
+
+TEST_F(ThreeSumClosestTest, Example4) {
+    vector<int> nums = {10,20,30,40,50,60,70,80,90};
+    int target = 1;
+    EXPECT_EQ(solution.threeSumClosest(nums, target), 60);
+}
+
+TEST_F(ThreeSumClosestTest, Example5) {
+    vector<int> nums = {-1000,-1000,-1000};
+    int target = 10000;
+    EXPECT_EQ(solution.threeSumClosest(nums, target), -3000);
+}
+
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
