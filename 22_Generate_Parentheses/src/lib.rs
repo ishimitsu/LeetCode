e5ed6b14626c274a83d926edf136a8126c82feb3
Mitsutaka Shiraishi <srismttk@gmail.com>
2026-01-12 16:38:23 +0900

    refactoring: remove some clone()

--- 22_Generate_Parentheses/src/lib.rs
+++ 22_Generate_Parentheses/src/lib.rs
@@ -1,26 +1,29 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn get_all_parenthesis (parenthesis: String, n: i32, left: i32, right: i32, result: &mut Vec<String>) {
+    pub fn get_all_parenthesis(parenthesis: &mut String, n: i32, left: i32, right: i32, result: &mut Vec<String>) {
 
         if left < n {
-            Self::get_all_parenthesis (parenthesis.clone() + "(", n, left + 1, right, result);
+            parenthesis.push('(');
+            Self::get_all_parenthesis (parenthesis, n, left + 1, right, result);
+            parenthesis.pop(); // remove ( to pass original parenthesis to under.
         }
 
-        if right < left && right < n {
-            Self::get_all_parenthesis (parenthesis.clone() + ")", n, left, right + 1, result);
+        if right < left {
+            parenthesis.push(')');
+            Self::get_all_parenthesis (parenthesis, n, left, right + 1, result);
+            parenthesis.pop(); // remove ( to pass original parenthesis to under.
         }
 
         if right == n {
-            result.push (parenthesis);
+            result.push (parenthesis.clone());
         }
     }
 
     pub fn generate_parenthesis(n: i32) -> Vec<String> {
-        // TODO: implement
         let mut parenthesis: Vec<String> = Vec::new();
-        Self::get_all_parenthesis("".to_string(), n, 0, 0, &mut parenthesis);
-        //println!("{:?}", parenthesis);
+        let mut dummy: String = "".to_string();
+        Self::get_all_parenthesis(&mut dummy, n, 0, 0, &mut parenthesis);
         parenthesis
     }
 }
