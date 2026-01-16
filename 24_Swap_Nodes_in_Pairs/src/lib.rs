cfeb32d5fd5636aa96a4775afe0091accdd109b1
Mitsutaka Shiraishi <srismttk@gmail.com>
2026-01-16 22:34:09 +0900

    passed local-test and Submit-tests.

--- 24_Swap_Nodes_in_Pairs/src/lib.rs
+++ 24_Swap_Nodes_in_Pairs/src/lib.rs
@@ -16,8 +16,22 @@ pub struct Solution;
 
 impl Solution {
     pub fn swap_pairs(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        // TODO: implement
-        head
+        let mut dummy = Box::new(ListNode { val: 0, next: head });
+        let mut prev = &mut dummy;
+
+        while prev.next.is_some() && prev.next.as_ref().unwrap().next.is_some() {
+            let mut first = prev.next.take().unwrap();
+            let mut second = first.next.take().unwrap();
+
+            first.next = second.next.take();
+            second.next = Some(first);
+            prev.next = Some(second);
+
+            // prev goes to first->second->next
+            prev = prev.next.as_mut().unwrap().next.as_mut().unwrap();
+        }
+
+        dummy.next
     }
 }
 
