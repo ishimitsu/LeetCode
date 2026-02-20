10457c845db2977a293f361ee7985e40fcd0ce58
Mitsutaka Shiraishi <srismttk@gmail.com>
2026-02-20 22:22:39 +0900

    passed LeetCode all testcase

--- 25_Reverse_Nodes_in_k-Group/src/lib.rs
+++ 25_Reverse_Nodes_in_k-Group/src/lib.rs
@@ -40,20 +40,22 @@ impl Solution {
                 k_head.next = k_next.next.take();
                 k_next.next = prev.next.take();
                 prev.next = Some(k_next);
-                println!("a {}", prev.val);
             }
 
-            // Need to connect end of k-1 -> k_head
-            let mut k_end = prev.next.as_mut().unwrap();
-            for _ in 0..k-2 {
-                k_end = k_end.next.as_mut().unwrap();
+            // prev of k_head is none, so need to connect end of k and k_head
+            if k > 1 {
+                let mut k_end = prev.next.as_mut().unwrap();
+                for _ in 0..k-2 {
+                    k_end = k_end.next.as_mut().unwrap();
+                }
+                k_end.next = Some(k_head);
+            } else {
+                prev.next = Some(k_head);
             }
-            k_end.next = Some(k_head);
 
             // prev goes to next k node
             for _ in 0..k {
                 prev = prev.next.as_mut().unwrap(); // move prev to k
-                println!("b {}", prev.val);
             }
 
         }
@@ -103,5 +105,14 @@ mod tests {
         let head = vec_to_list(vec![1, 2, 3, 4, 5]);
         let result = Solution::reverse_k_group(head, 3);
         assert_eq!(list_to_vec(result), vec![3, 2, 1, 4, 5]);
+    }
+
+    #[test]
+    fn example3() {
+        // Input: head = [1,2,3,4,5], k = 3
+        // Output: [3,2,1,4,5]
+        let head = vec_to_list(vec![1, 2, 3, 4, 5]);
+        let result = Solution::reverse_k_group(head, 1);
+        assert_eq!(list_to_vec(result), vec![1, 2, 3, 4, 5]);
     }
 }
