882b50c43e0cfab144893b52bfe707565ff6b8b9
Mitsutaka Shiraishi <srismttk@gmail.com>
2026-02-22 01:14:03 +0900

    passed LeetCode Submit, but still slow

--- 53_Maximum_Subarray/src/lib.rs
+++ 53_Maximum_Subarray/src/lib.rs
@@ -1,23 +1,45 @@
+use core::num;
+use std::i32;
+
 pub struct Solution;
 
 impl Solution {
-    pub fn max_sub_array(nums: Vec<i32>) -> i32 {
-        // TODO: implement
-        if nums.len() < 0 { return 0;}
-        else if nums.len() == 1 { return nums[0]; }
+    pub fn get_max_sub_array (nums: &Vec<i32>, left: usize, right: usize) -> i32 {
+        if left == right { return nums[left]; }
 
-        let mut max_sum = nums[0];
-        for left in 0..nums.len() {
-            for right in (left+1)..(nums.len()+1) {
-                let sum = nums[left..right].iter().sum();
-                println!("left:{}, right:{}, cur:{}, max:{}", left, right, sum, max_sum);
-                if sum > max_sum {
-                    max_sum = sum;
-                }
-            }
+        let mid = (left + right) / 2;
+        //println!("left:{}, right:{}, mid:{}", left, right, mid);
+        let left_max = Self::get_max_sub_array(&nums, left, mid);
+        let right_max = Self::get_max_sub_array(&nums, mid+1, right);
+
+        let mut cross_max_left = i32::MIN;
+        let mut cross_max_right = i32::MIN;
+
+        let mut cross_sum_left = 0;
+        for l in (left..mid+1).rev() {
+            cross_sum_left += nums[l];
+            cross_max_left = if cross_sum_left > cross_max_left { cross_sum_left } else { cross_max_left };
         }
+        let mut cross_sum_right = 0;
+        for r in (mid+1)..(right+1) {
+            cross_sum_right += nums[r];
+            cross_max_right = if cross_sum_right > cross_max_right { cross_sum_right } else { cross_max_right };
+        }
+        let cross_max = cross_max_left + cross_max_right;
+
+        let mut max_sum;
+        max_sum = if left_max > right_max { left_max } else { right_max };
+        max_sum = if cross_max > max_sum { cross_max } else { max_sum };
+        //println!("left:{}, right:{}, max:{} {} {} {}", left, right, left_max, right_max, cross_max, max_sum);
 
         max_sum
+    }
+
+    pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+        // TODO: implement
+        if nums.is_empty() { return 0;}
+        else if nums.len() == 1 { return nums[0]; }
+        Self::get_max_sub_array(&nums, 0, nums.len()-1)
     }
 }
 
