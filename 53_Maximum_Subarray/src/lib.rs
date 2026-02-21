798de816c4ea90031517a256ab36b81e0506325c
Mitsutaka Shiraishi <srismttk@gmail.com>
2026-02-21 23:08:52 +0900

    passed local test, but too slow.

--- 53_Maximum_Subarray/src/lib.rs
+++ 53_Maximum_Subarray/src/lib.rs
@@ -3,7 +3,21 @@ pub struct Solution;
 impl Solution {
     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
         // TODO: implement
-        0
+        if nums.len() < 0 { return 0;}
+        else if nums.len() == 1 { return nums[0]; }
+
+        let mut max_sum = nums[0];
+        for left in 0..nums.len() {
+            for right in (left+1)..(nums.len()+1) {
+                let sum = nums[left..right].iter().sum();
+                println!("left:{}, right:{}, cur:{}, max:{}", left, right, sum, max_sum);
+                if sum > max_sum {
+                    max_sum = sum;
+                }
+            }
+        }
+
+        max_sum
     }
 }
 
@@ -30,5 +44,10 @@ mod tests {
         // Input: nums = [5,4,-1,7,8]
         // Output: 23
         assert_eq!(Solution::max_sub_array(vec![5, 4, -1, 7, 8]), 23);
+    }
+
+    #[test]
+    fn example4() {
+        assert_eq!(Solution::max_sub_array(vec![-2, -1]), -1);
     }
 }
