be4ed4c9e86fec44c250a8930491e09f684efee3
Mitsutaka Shiraishi <srismttk@gmail.com>
2026-03-07 00:04:09 +0900

    add test3, and add sort_by_key due to sort intervals.

--- 56_Merge_Intervals/src/lib.rs
+++ 56_Merge_Intervals/src/lib.rs
@@ -3,7 +3,16 @@ pub struct Solution;
 impl Solution {
     pub fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
         // TODO: implement
-        vec![]
+        let mut result = intervals;
+        if result.len() == 0 { return result; }
+
+        println!("{:?}", result);
+
+        result.sort_by_key(|interval| interval[0]);
+
+        println!("{:?}", result);
+
+        result
     }
 }
 
@@ -12,9 +21,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn example1() {
-        // Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
-        // Output: [[1,6],[8,10],[15,18]]
+    fn test1() {
         let intervals = vec![vec![1, 3], vec![2, 6], vec![8, 10], vec![15, 18]];
         assert_eq!(
             Solution::merge(intervals),
@@ -23,10 +30,14 @@ mod tests {
     }
 
     #[test]
-    fn example2() {
-        // Input: intervals = [[1,4],[4,5]]
-        // Output: [[1,5]]
+    fn test2() {
         let intervals = vec![vec![1, 4], vec![4, 5]];
         assert_eq!(Solution::merge(intervals), vec![vec![1, 5]]);
+    }
+
+    #[test]
+    fn test3() {
+        let intervals = vec![vec![4, 7], vec![1, 4]];
+        assert_eq!(Solution::merge(intervals), vec![vec![1, 7]]);
     }
 }
