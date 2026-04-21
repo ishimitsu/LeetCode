5c04df3822686d9ae02582c6a45407d3ae65a381
Mitsutaka Shiraishi <srismttk@gmail.com>
2026-04-21 23:32:15 +0900

    feat: implement insert that passes tests and submitted.

--- 57_Insert_Interval/src/lib.rs
+++ 57_Insert_Interval/src/lib.rs
@@ -3,7 +3,28 @@ pub struct Solution;
 impl Solution {
     pub fn insert(intervals: Vec<Vec<i32>>, new_interval: Vec<i32>) -> Vec<Vec<i32>> {
         // TODO: implement
-        vec![]
+        let mut sorted = intervals;
+         // push new interval into intervals and sort
+        sorted.push(new_interval);
+        sorted.sort_by_key(|interval| interval[0]);
+
+        let mut result: Vec<Vec<i32>> = vec![];
+
+        for interval in sorted {
+
+            if result.is_empty() || interval[0] > result.last().unwrap()[1] {
+                // not duplicate, and interval is bigger than result
+                result.push(interval);
+            } else if interval[1] > result.last().unwrap()[1] {
+                // After sorted, if duplicated interval[1] is smaller than result[1],
+                // it is always covered by result
+                // so only check interval[1] bigger case, and update result[1]
+
+                result.last_mut().unwrap()[1] = interval[1];
+            }
+        }
+
+        result
     }
 }
 
