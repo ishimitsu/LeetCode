9ce734c7af26b829a1bb95f0ae980c6dc9e24444
Mitsutaka Shiraishi <srismttk@gmail.com>
2026-04-24 02:01:16 +0900

    feat: clean can_jump, and recreate it with Greedy Algorithm.

--- 55_Jump_Game/src/lib.rs
+++ 55_Jump_Game/src/lib.rs
@@ -2,15 +2,24 @@ pub struct Solution;
 
 impl Solution {
     pub fn can_jump(nums: Vec<i32>) -> bool {
-        // TODO: implement
-        let mut cur_idx = 0;
-        let last_idx = nums.len().try_into().unwrap();
-        for jmp_num in nums {
-            if jmp_num <= 0 { return false }
-            cur_idx = cur_idx + jmp_num;
+        if nums.len() == 1 { return true }
+        let last_idx = nums.len() - 1;
+        let mut cur_reach_max = 0;
+
+        // implemented with Greedy Algorithm
+        for idx in 0..last_idx {
+            if idx > cur_reach_max { return false } // can't reach max
+
+            let jmp_idx = idx + nums[idx] as usize;  // elements in nums always 0>=0
+            if jmp_idx > cur_reach_max {
+                // if reach_max is updated, you can jump all 0..reach_max
+                // so you only need to check reach_max
+                cur_reach_max = jmp_idx;
+                if cur_reach_max >= last_idx { break }
+            }
         }
 
-        cur_idx >= last_idx
+        cur_reach_max >= last_idx
     }
 }
 
@@ -30,5 +39,17 @@ mod tests {
         // Input: nums = [3,2,1,0,4]
         // Output: false
         assert_eq!(Solution::can_jump(vec![3, 2, 1, 0, 4]), false);
+    }
+
+    #[test]
+    fn example3() {
+        // Output: true
+        assert_eq!(Solution::can_jump(vec![1]), true);
+    }
+
+        #[test]
+    fn example4() {
+        // Output: true
+        assert_eq!(Solution::can_jump(vec![2, 5, 0, 0]), true);
     }
 }
