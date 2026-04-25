adaabdfbb3921fd67ba53d17217f787a6b8b0332
Mitsutaka Shiraishi <srismttk@gmail.com>
2026-04-26 03:02:49 +0900

    feat: passed localtest, but still failed Submit

--- 45_Jump_Game_II/src/lib.rs
+++ 45_Jump_Game_II/src/lib.rs
@@ -3,7 +3,33 @@ pub struct Solution;
 impl Solution {
     pub fn jump(nums: Vec<i32>) -> i32 {
         // TODO: implement
-        0
+        if nums.len() == 1 { return 0 }
+        let last_idx = nums.len() - 1;
+        let mut reach_max = 0;
+        let mut jmp_times = 0;
+        let mut min_jmp_times = last_idx as i32;
+
+        // implemented with Greedy Algorithm
+        for idx in 0..last_idx {
+            //if idx > reach_max { return -1 } // It's guaranteed that you can reach last_idx
+
+            let jmp_idx = idx + nums[idx] as usize;  //nums are always >=0, so convert i32 => usize
+            jmp_times = jmp_times + 1;
+            if jmp_idx > reach_max {
+                // if reach_max is updated, you can jump all 0..reach_max
+                // so you only need to check reach_max
+                reach_max = jmp_idx;
+
+                if reach_max >= last_idx {
+                    if jmp_times < min_jmp_times { min_jmp_times = jmp_times; }
+                    println!("{}, {}, {}. {}", jmp_idx, reach_max, jmp_times, min_jmp_times);
+
+                    break;
+                }
+            }
+        }
+
+        min_jmp_times
     }
 }
 
