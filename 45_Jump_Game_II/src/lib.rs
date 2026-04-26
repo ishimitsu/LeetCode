3eea548be8e193ca01bb0fbacb95b103057b3f7d
Mitsutaka Shiraishi <srismttk@gmail.com>
2026-04-26 19:26:17 +0900

    feat: passed Subimit.

--- 45_Jump_Game_II/src/lib.rs
+++ 45_Jump_Game_II/src/lib.rs
@@ -4,32 +4,28 @@ impl Solution {
     pub fn jump(nums: Vec<i32>) -> i32 {
         // TODO: implement
         if nums.len() == 1 { return 0 }
-        let last_idx = nums.len() - 1;
-        let mut reach_max = 0;
+        let last_idx = nums.len()-1;
         let mut jmp_times = 0;
-        let mut min_jmp_times = last_idx as i32;
+        let mut cur_jmp_reach_max: usize = 0;
+        let mut next_jmp_reach_max = 0;
 
         // implemented with Greedy Algorithm
         for idx in 0..last_idx {
             //if idx > reach_max { return -1 } // It's guaranteed that you can reach last_idx
 
-            let jmp_idx = idx + nums[idx] as usize;  //nums are always >=0, so convert i32 => usize
-            jmp_times = jmp_times + 1;
-            if jmp_idx > reach_max {
+            let jmp_idx: usize = idx + nums[idx] as usize;  //nums are always >=0, so convert i32 => usize
+            if jmp_idx > next_jmp_reach_max {
                 // if reach_max is updated, you can jump all 0..reach_max
                 // so you only need to check reach_max
-                reach_max = jmp_idx;
-
-                if reach_max >= last_idx {
-                    if jmp_times < min_jmp_times { min_jmp_times = jmp_times; }
-                    println!("{}, {}, {}. {}", jmp_idx, reach_max, jmp_times, min_jmp_times);
-
-                    break;
-                }
+                next_jmp_reach_max = jmp_idx;
+            }
+            if idx == cur_jmp_reach_max {
+                jmp_times = jmp_times + 1;
+                cur_jmp_reach_max = next_jmp_reach_max;
             }
         }
 
-        min_jmp_times
+        jmp_times
     }
 }
 
@@ -39,15 +35,22 @@ mod tests {
 
     #[test]
     fn example1() {
-        // Input: nums = [2,3,1,1,4]
-        // Output: 2
         assert_eq!(Solution::jump(vec![2, 3, 1, 1, 4]), 2);
     }
 
     #[test]
     fn example2() {
-        // Input: nums = [2,3,0,1,4]
-        // Output: 2
-        assert_eq!(Solution::jump(vec![2, 3, 0, 1, 4]), 2);
+        assert_eq!(Solution::jump(vec![1,2]), 1);
     }
+
+    #[test]
+    fn example3() {
+        assert_eq!(Solution::jump(vec![1,2,1,1,1]), 3);
+    }
+
+    #[test]
+    fn example4() {
+        assert_eq!(Solution::jump(vec![7,0,9,6,9,6,1,7,9,0,1,2,9,0,3]), 2);
+    }
+
 }
