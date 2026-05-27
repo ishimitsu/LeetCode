1b3d301edeb6200041276d659015a5124a1b9875
Mitsutaka Shiraishi <srismttk@gmail.com>
2026-05-28 02:05:17 +0900

    feat: implement min_jumps that passes local tests.

--- 1345_Jump_Game_IV/src/lib.rs
+++ 1345_Jump_Game_IV/src/lib.rs
@@ -3,6 +3,50 @@ pub struct Solution;
 impl Solution {
     pub fn min_jumps(arr: Vec<i32>) -> i32 {
         // TODO: implement
+
+        if arr.len() <= 1 { return 0 }
+        let mut mov_cnt = 0;
+        let mut cur_idx = 0;
+        let arr_len = arr.len();
+        let mut visited_idx = vec![false; arr.len()];
+        let mut queue = vec![];
+        queue.push( (cur_idx, mov_cnt) );
+        visited_idx[cur_idx] = true;
+
+        // BFS algorithm
+        while !queue.is_empty() {
+            (cur_idx, mov_cnt) = queue.pop().unwrap();
+            mov_cnt = mov_cnt + 1;
+
+            if cur_idx + 1 < arr_len {
+                let forward_idx = cur_idx + 1;
+                if visited_idx[forward_idx] == false {
+                    if forward_idx == arr_len - 1 { return mov_cnt}
+                    visited_idx[forward_idx] = true;
+                    queue.push( (forward_idx, mov_cnt) );
+                }
+            }
+
+            if cur_idx >= 1 {
+                let back_idx: usize = cur_idx - 1;
+                if visited_idx[back_idx] == false {
+                    if back_idx == arr_len - 1 { return mov_cnt }
+                    visited_idx[back_idx] = true;
+                    queue.push( (back_idx, mov_cnt) );
+                }
+            }
+
+            for same_val_idx in 0..arr_len {
+                if visited_idx[same_val_idx] == false
+                && same_val_idx != cur_idx
+                && arr[same_val_idx] == arr[cur_idx] {
+                    if same_val_idx == arr_len - 1 { return mov_cnt }
+                    visited_idx[same_val_idx] = true;
+                    queue.push( (same_val_idx, mov_cnt) );
+                }
+            }
+        }
+
         -1
     }
 }
